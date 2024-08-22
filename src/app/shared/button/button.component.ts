eb26266d4491b29ec240db31bd15d9c2bd8fd29b
Hrushikesh <kajalehrushikesh@gmail.com>
2024-08-22 21:39:54 +0530

    first commit

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button[appButton]', //this is called attribute selector , 
+  //button having attribute appButton this component will be triggered
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
