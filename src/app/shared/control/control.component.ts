f0e82eadf7debe8900f9b63a3dfda2994bc53c12
Hrushikesh <kajalehrushikesh@gmail.com>
2024-08-25 11:31:24 +0530

    This changes include some concepts like ngContent

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,12 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding, HostListener, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host : {
+    class : 'control',
+    '(click)':'onClick()'
+  }
 })
 export class ControlComponent {
+//@HostBinding('class') class = "control";
+
 @Input({required: true}) label!:string;
+
+// @HostListener('click')  onClick()
+// {
+//   console.log('Clicked..')
+// }
+
+onClick()
+{
+  console.log('Clicked..')
+}
 }
