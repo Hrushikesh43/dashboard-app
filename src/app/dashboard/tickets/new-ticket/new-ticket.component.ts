deb1dd3453706fc0a12f3315fc6b308ef26febe7
Hrushikesh <kajalehrushikesh@gmail.com>
2024-09-12 09:42:57 +0530

    Last commit of this project

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -10,12 +10,20 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements AfterViewInit, OnInit {
+  @Output() add = new EventEmitter<{ title: string, text: string }>();
   @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  ngOnInit() {
+    console.log(this.form?.nativeElement);
+  }
+  ngAfterViewInit(){
+    console.log('After View Init');
+    console.log(this.form?.nativeElement);
+  }
+
 onSubmit(title: string, ticketText: string, )
 {
-  console.log(title);
-  console.log(ticketText);
+  this.add.emit({title: title, text: ticketText});
   this.form?.nativeElement.reset();
 }
 }
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, ElementRef, HostBinding, HostListener, Input, ViewEncapsulation } from '@angular/core';
+import { AfterContentInit, afterNextRender, afterRender, Component, ContentChild, ElementRef, HostBinding, HostListener, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -12,10 +12,21 @@ import { Component, ContentChild, ElementRef, HostBinding, HostListener, Input, 
     '(click)': 'onClick()'
   }
 })
-export class ControlComponent {
+export class ControlComponent implements AfterContentInit {
   @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {
+    afterRender(()=> {
+    console.log('After Render');
+    })
+    afterNextRender(()=>{
+    console.log('After Next Render');
+    })
+   }
   //@HostBinding('class') class = "control";
+
+  ngAfterContentInit() {
+    console.log('NgAfterContentInit');
+  }
 
   @Input({ required: true }) label!: string;
 
